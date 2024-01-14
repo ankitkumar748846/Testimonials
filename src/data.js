238709e81d005cb8409d8d65bc4b275423cde78f
ankit <ankitkumar748846@gmail.com>
2024-01-15 02:00:34 +0530

    react

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Ankit Kumar",
+    job: "Software Engineer",
+    image: "https://imgur.com/SzSmUG4.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Kavi Khalique",
+    job: "Software Engineer",
+    image: "https://imgur.com/nAnJ7DU.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Prashan Kumar",
+    job: "Marketing Manager",
+    image: "https://imgur.com/9TvmTac.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Arnab Das",
+    job: "Content Strategist",
+    image: "https://imgur.com/JXkK1Hz.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Aryan Rai",
+    job: "Data Analyst",
+    image: "https://imgur.com/2HtyS78.jpg",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
